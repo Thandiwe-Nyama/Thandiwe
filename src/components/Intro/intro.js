e51a0455bafbbc55b4e9a4d7c2aec57cbbf56dd5
Thandiwe-Nyama <158051076+Thandiwe-Nyama@users.noreply.github.com>
2024-07-15 16:01:15 +0200

    update

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assest/image';
+import bg from '../../assets/image (1).png';
+import btnImg from '../../assets/hire-me.png';
+import { Link } from 'react-scroll';
 
 const Intro = () => {
   return (
     <section id="intro">
       <div className="introContent">
-
+        <span className="hello">Hello,</span>
+        <span className="introText">I'm, <span className="introName">Thandiwe</span> <br />Website Designer</span>
+        <p className="introPara">As a creative website designer with a background in design and development, I craft visually stunning, functional websites that perform seamlessly on all devices.</p>
+        <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg'/>Hire Me</button></Link>
       </div>
       <img src={bg} alt="Profile" className="bg" />
     </section>
   )
 }
 
-export default Intro
+export default Intro;
