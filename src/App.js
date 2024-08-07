076c97bf508cd433d00f8a7ab0d8a8fed26297ae
Thandiwe Nyama <thandiwenyama2@gmail.com>
2024-08-07 18:53:58 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar/navbar";
-import Intro from './components/Intro/Intro'; 
+import Intro from './components/Intro/intro'; 
 import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 
