7c5924f925a8580de06dc7a48c4d6fdcdbf2cd82
Thandiwe Nyama <thandiwenyama2@gmail.com>
2024-08-13 15:19:56 +0200

    update

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -21,13 +21,13 @@ const Contact = () => {
                     <img src={Phathek} alt="" className="clientImg" />
                 </div>
                 </div>
-            <div id="contact">
+            <div id="contactPage">
                 <h1 className="contactPageTitle">Contact Me</h1>
                 <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
                 <form className="contactForm" >
                     <input type="text" className="name" placeholder="Your Name" />
                     <input type="email" className="email" placeholder="Your Email" />
-                    <textarea className="mgs" name="message" rows="5" placeholder="Your Message"></textarea>
+                    <textarea className="msg" name="message" rows="5" placeholder="Your Message"></textarea>
                     <button type='submit' value="send" className="submitBtn">Submit</button>
                     <div className="links">
                         <img src={facebook} alt="" classname="link" />
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,19 +3,30 @@ import './navbar.css'
 import logo from '../../assets/logo2.png';
 import contactImg from '../../assets/contact.png';
 import { Link } from 'react-scroll';
+import menu from '../../assets/icons8-menu-50.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
         <img src={logo} alt="logo" className='logo'/>
         <div className="desktopMenu">
-            <Link className="desktopMenuListItem">Home</Link>
-            <Link className="desktopMenuListItem">About Me</Link>
-            <Link className="desktopMenuListItem">Portfolio</Link>
-            <Link className="desktopMenuListItem">Clients</Link>
+            <Link activeclass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
+            <Link activeclass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">Skills</Link>
+            <Link activeclass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">Portfolio</Link>
+            <Link activeclass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">Clients</Link>
         </div>
-        <button className="desktopMenuBtn">
-            <img src={contactImg} alt="" className="desktopMenuImg" />Contact Me</button> 
+        <button className="desktopMenuBtn" onClick={() => {
+          document.getElementsById('contact').scrollIntroView({behavior: 'smooth'});
+        }}>
+            <img src={contactImg} alt="" className="desktopMenuImg" />Contact Me</button>
+
+             <img src={menu} alt="Menu" className='mobMenu'/>
+        <div className="desktopMenu">
+            <Link activeclass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
+            <Link activeclass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">Skills</Link>
+            <Link activeclass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">Portfolio</Link>
+            <Link activeclass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">Clients</Link>
+        </div> 
     </nav>
   )
 }
